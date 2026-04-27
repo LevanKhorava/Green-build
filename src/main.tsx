b6385ece8e51baea354b024899cbb65d841d7b81
Levan Khorava <levan.khorava@makingscience.com>
2026-04-27 16:41:53 +0400

    add vercel config

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/Green-build/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
